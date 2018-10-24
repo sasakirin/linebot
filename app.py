2a5907400096ffff81b51ec848a3c063eb5ee74c
sasakirin <43978474+sasakirin@users.noreply.github.com>
2018-10-24 15:08:35 +0800

    Update app.py

--- app.py
+++ app.py
@@ -43,7 +43,7 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    message = TextSendMessage(text=(event.sourse.user_id)) #reply userid
+    message = TextSendMessage(text=(event.source.user_id)) #reply userid
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
